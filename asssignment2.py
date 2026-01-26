db5a719a130b2c62ce035bfb2e09c979017e7272
Sindhu <sindhu@Sairams-MacBook-Pro.local>
2026-01-26 11:55:52 -0500

    project and instructions added

--- asssignment2.py
+++ asssignment2.py
@@ -4,7 +4,7 @@ Collaborative Python Assignment
 """
 
 def main():
-    print("Welcome to the Health Application Demo\n")
+    print("Welcome to the Assignement2.1\n")
 
     # ==== STUDENT FUNCTION CALLS ====
     # Add your function call below this line
